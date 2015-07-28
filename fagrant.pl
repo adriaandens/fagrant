603c40ac817565353df568b5ef252bb13d7e242e
Adriaan Dens <adriaan.dens@gmail.com>
2015-07-29 01:34:59 +0200

    fixed some tiny issues

--- fagrant.pl
+++ fagrant.pl
@@ -1,18 +1,20 @@
 #!/usr/bin/perl
+use strict;
+use warnings;
 
 print "\nPlease install VirtualBox first...\n\n" if system("VBoxManage -v > /dev/null 2>&1") != 0;
 my $cwd_vm;
 if(-e 'FagrantFile') {
     open(FH, '< FagrantFile');
-    $cwd_vm = <FH>;
-    chomp($cwd_vm);
+    chomp($cwd_vm = <FH>);
     close(FH);
     if(!vm_state($cwd_vm, 'exists')) {
         print "VM from FagrantFile doesn't exist. :x\n";
         exit 1;
     }
 }
-$ARGV[0] && $ARGV[0] =~ /(init|up|ssh|halt|destroy)/ ? $ARGV[0]() : help();
+my $subroutines = {'init' => \&init, 'up' => \&up, 'ssh' => \&ssh, 'halt' => \&halt, 'destroy' => \&destroy};
+$ARGV[0] && $ARGV[0] =~ /(init|up|ssh|halt|destroy)/ ? $subroutines->{$ARGV[0]}->() : help();
 
 sub vm_state {
     my ($vm_name, $state) = @_;
@@ -30,18 +32,18 @@ sub vm_state {
 
 sub init {
     if($cwd_vm) {
-        print "Wow wow, there's already a fagrant box for this directory?! Are you crazy?\n";
+        print "Wow wow, there's already a fagrant VM for this directory?! Are you crazy?\n";
         return;
     }
     if(vm_state($ARGV[1], "exists")) {
         $cwd_vm = $ARGV[1] . "_" . time();
         print "Cloning VM with name '$cwd_vm'.\n";
-        print `VBoxManage clonevm $ARGV[1] --name $cwd_vm`;
+        print `VBoxManage clonevm $ARGV[1] --name "$cwd_vm"`;
         my @vmdir = map {/Default machine folder:\s+(.+)$/;$1} grep {/^Default machine folder:/} `VBoxManage list systemproperties`;
         my $vm_location = $vmdir[0] . '/' . $cwd_vm . '/' . $cwd_vm . '.vbox';
         print `VBoxManage registervm "$vm_location"`;
         
-        open(FH, "> FagrantFile") and print FH "$cwd_vm" and close(FH);
+        open(FH, "> FagrantFile") and print FH $cwd_vm and close(FH);
     } else {
         print "That VM doesn't exist...\n";
     }
@@ -55,24 +57,20 @@ sub up {
         `VBoxManage sharedfolder add "$cwd_vm" --name "guestfolder" --hostpath $ENV{PWD} --automount > /dev/null 2>&1`;
         print "Booting VM...\n";
         system("VBoxHeadless --startvm \"$cwd_vm\" > /dev/null 2>&1 &");
-        sleep(15);
+        sleep(15); # Find a better way?
     }
 }
 
 sub ssh {
-    my $user = $ARGV[1] // "fagrant"; # http://www.effectiveperlprogramming.com/2010/10/set-default-values-with-the-defined-or-operator/
+    my $user = $ARGV[1] // "fagrant";
     my $keyfile = $user eq 'vagrant' ? $ENV{HOME} . '/.vagrant.d/insecure_private_key' : $ENV{HOME} . '/.ssh/fagrant';
-    my @sshrules = map {/host port = (\d+),/;$1} grep {/NIC \d+ Rule.+guest port = 22/} `VBoxManage showvminfo $cwd_vm`;
-    if($cwd_vm && vm_state($cwd_vm, 'running')) {
-        system("ssh -q -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $keyfile $user\@localhost -p " . $sshrules[0]);
-    }
+    my @sshrules = map {/host port = (\d+),/;$1} grep {/NIC \d+ Rule.+guest port = 22/} `VBoxManage showvminfo "$cwd_vm"`;
+    system("ssh -q -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $keyfile $user\@localhost -p " . $sshrules[0]) if $cwd_vm && vm_state($cwd_vm, 'running');
 }
 
 sub halt {
-    my $method = $ARGV[1] eq '--force' ? 'poweroff' : 'acpipowerbutton';
-    if($cwd_vm && vm_state($cwd_vm, 'running')) {
-        `VBoxManage controlvm "$cwd_vm" $method`;
-    }
+    my $method = $ARGV[1] && $ARGV[1] eq '--force' ? 'poweroff' : 'acpipowerbutton';
+    `VBoxManage controlvm "$cwd_vm" $method` if $cwd_vm && vm_state($cwd_vm, 'running');
 }
 
 sub destroy {
@@ -80,10 +78,10 @@ sub destroy {
         halt() if vm_state($cwd_vm, 'running');
         if(not defined $ARGV[1]) {
             print "Not so fast José! U sure? "; # I know myself, I need this.
-            chomp(my $w = <STDIN>);
-            `VBoxManage unregistervm $cwd_vm --delete` if $w =~ /^ye?s?/i;
+            chomp(my $answer = <STDIN>);
+            `VBoxManage unregistervm "$cwd_vm" --delete` if $answer =~ /^ye?s?/i;
         }
-        `VBoxManage snapshot restorecurrent $cwd_vm` if $ARGV[1] eq '--revert';
+        `VBoxManage snapshot restorecurrent "$cwd_vm"` if $ARGV[1] && $ARGV[1] eq '--revert';
         unlink('FagrantFile');
     }
 }
