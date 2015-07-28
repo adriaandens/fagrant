6576a7dcccd08cf7777d00d91150256eee6bb8e8
Adriaan Dens <adriaan.dens@gmail.com>
2015-07-28 14:30:09 +0200

    It's actually working

--- fagrant.pl
+++ fagrant.pl
@@ -1,3 +1,5 @@
+#!/bin/perl
+
 print "\nPlease install VirtualBox first...\n\n" if system("VBoxManage -v > /dev/null 2>&1") != 0;
 my $cwd_vm;
 if(-e 'FagrantFile') {
@@ -5,31 +7,26 @@ if(-e 'FagrantFile') {
     $cwd_vm = <FH>;
     chomp($cwd_vm);
     close(FH);
-    # Check if VM exists...
     if(!vm_state($cwd_vm, 'exists')) {
         print "VM from FagrantFile doesn't exist. :x\n";
         exit 1;
     }
 }
+$ARGV[0] && $ARGV[0] =~ /(add|init|up|ssh|halt|destroy)/ ? $ARGV[0]() : help();
 
 sub vm_state {
     my ($vm_name, $state) = @_;
 
     my @lines;
-    if($state eq 'exists') {@lines = `VBoxManage list vms`;}
-    elsif($state eq 'running') {@lines = `VBoxManage list runningvms`;}
-
-    my $found = 0;
-    foreach(@lines) {
-        /^"(.+)"\s{1}[^"]+$/;
-        if($vm_name eq $1) {
-            $found = $vm_name;
-            break;
-        }
+    if($state eq 'exists') {
+        @lines = `VBoxManage list vms`;
+    } elsif($state eq 'running') {
+        @lines = `VBoxManage list runningvms`;
     }
+
+    my $found = grep {/^"(.+)"\s{1}[^"]+$/;$vm_name eq $1} @lines;
     return $found;
 }
-$ARGV[0] && $ARGV[0] =~ /(add|init|up|ssh|halt|destroy)/ ? $ARGV[0]() : help();
 
 sub init {
     if($cwd_vm) {
@@ -38,14 +35,14 @@ sub init {
     }
     if(vm_state($ARGV[1], "exists")) {
         $cwd_vm = $ARGV[1] . "_" . time();
-        print "Cloning VM with name '$cwd_vm'\n";
+        print "Cloning VM with name '$cwd_vm'.\n";
         print `VBoxManage clonevm $ARGV[1] --name $cwd_vm`;
         my @vmfolder = `VBoxManage list systemproperties`;
         my @vmdir = map {/Default machine folder:\s+(.+)$/;$1} grep {/^Default machine folder:/} @vmfolder;
         my $vmd = $vmdir[0];
+        #my $vmd = ${map {/Default machine folder:\s+(.+)$/;$1} grep {/^Default machine folder:/} `VBoxManage list systemproperties`}[0];
         print `VBoxManage registervm "${vmd}/${cwd_vm}/${cwd_vm}.vbox"`;
         
-        # Store mapping in file in current working directory
         open(FH, "> FagrantFile") and print FH $cwd_name and close(FH);
     } else {
         print "That VM doesn't exist...\n";
@@ -54,33 +51,29 @@ sub init {
 
 sub up {
     if($cwd_vm) {
-        `VBoxHeadless --startvm "$cwd_vm"`;
-        `VBoxManage modifyvm "$cwd_vm" --natpf1 "guestssh,tcp,,2222,,22"`;
-        `VBoxManage sharedfolder add "$cwd_vm" --name "guestfolder" --hostpath $ENV{PWD}`
+        `VBoxManage modifyvm "$cwd_vm" --natpf1 "guestssh,tcp,,2222,,22" > /dev/null 2>&1`;
+        `VBoxManage sharedfolder add "$cwd_vm" --name "guestfolder" --hostpath $ENV{PWD} --automount > /dev/null 2>&1`;
+        print "Booting VM...\n";
+        system("VBoxHeadless --startvm \"$cwd_vm\" > /dev/null 2>&1 &");
+        sleep(15);
     }
 }
 
 sub ssh {
     if($cwd_vm && vm_state($cwd_vm, 'running')) {
-    # SSH into VM
-    # Give terminal to user
+        system("ssh -q -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i $ENV{HOME}/.ssh/id_rsa fagrant\@localhost -p 2222");
     }
 }
 
 sub halt {
-    # Maybe do something graceful first?
     if($cwd_vm && vm_state($cwd_vm, 'running')) {
         `VBoxManage controlvm $cwd_vm poweroff`;
     }
 }
 
 sub destroy {
-    if($cwd_vm && vm_state($cwd_vm, 'running')) {
-        halt();
-    }
-    #my @lines = `VBoxManage showvminfo $cwd_vm`;
-    #my @hdds = map {/UUID: ([^\)]+)\)/;$1} grep {/(IDE|SATA).+UUID:\s+([^\)]+)/i;} @lines;
     if($cwd_vm) {
+        halt() if vm_state($cwd_vm, 'running');
         `VBoxManage unregistervm $cwd_vm --delete`;
         unlink('FagrantFile');
     }
